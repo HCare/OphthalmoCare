d0510971dde4400087b8a7c0eb9542b3ab612f10
yass <yasser.moukhtar@gmail.com>
2014-10-07 14:11:17 +0200

    Added client side menu authorization

--- public/modules/roles/config/roles.client.config.js
+++ public/modules/roles/config/roles.client.config.js
@@ -4,8 +4,8 @@
 angular.module('roles').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Roles', 'roles', 'dropdown', '/roles(/create)?');
-		Menus.addSubMenuItem('topbar', 'roles', 'List Roles', 'roles');
-		Menus.addSubMenuItem('topbar', 'roles', 'New Role', 'roles/create');
+		Menus.addMenuItem('topbar', 'Roles', 'roles', 'dropdown', '/roles(/create)?', false, 0);
+		Menus.addSubMenuItem('topbar', 'roles', 'List Roles', 'roles', '/roles', false, 'list_roles', 0);
+		Menus.addSubMenuItem('topbar', 'roles', 'New Role', 'roles/create', '/roles/create', false, 'create_role', 1);
 	}
 ]);
